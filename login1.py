d2d2370faefed579fe471cce6a8b3793853e56db
luis c <lcedenonunez@Gmail.com>
2023-10-28 08:29:59 -0400

    venatana de inicio

--- login1.py
+++ login1.py
@@ -5,122 +5,114 @@ Created on Sat Jul 22 12:21:13 2023
 @author: Acer Tuch Screen
 """
 
-from PyQt5.QtWidgets import QApplication, QMainWindow, QFrame, QLabel, QPushButton, QLineEdit, QCheckBox, QMessageBox, QFormLayout, QHBoxLayout, QVBoxLayout, QGroupBox
-from PyQt5.QtCore import QSize
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFrame, QLabel, QPushButton, QLineEdit, QCheckBox, QWidget
+from PyQt5.QtWidgets import QMessageBox, QFormLayout, QHBoxLayout, QVBoxLayout, QGroupBox, QGraphicsView, QGraphicsScene, QGraphicsPixmapItem
+from PyQt5.QtCore import QSize, QEvent
 from PyQt5.QtGui import QPixmap, QFont, QIcon
 from NuevoRegistro import RegistroUsuario
 import VentanaPrincipal as vp
 from PyQt5.QtCore import Qt
 import sys
 
+class registrar(RegistroUsuario):
+    def __init__(self):
+        super(registrar,self).__init__()
+                
 
 class login(QMainWindow):
     def __init__(self):
         super(login,self).__init__()
-        self.resize(550,600)
+        self.setFixedSize(850,640)
+        self.setWindowTitle("User")
+       
         
-        Frame_Central = QFrame(self)
-        self.setCentralWidget(Frame_Central)
         
-        image_url = "imagen/acceso.png" # directorio de la imagen
+        
+        frame_padre = QFrame(self)
+        frame_padre.resize(425,600)
+        frame_padre.move(420,20)
+    
+        
+        image_url = "imagen/cliente.png" # directorio de la imagen
         
         try:
             with open(image_url):
-                label_image = QLabel(self)
+                label_image = QLabel(frame_padre)
                 pixmap = QPixmap(image_url)
-                pixmap.scaled(75,100)
                 label_image.setPixmap(pixmap)
-                label_image.resize(75,100)
-                label_image.setAlignment(Qt.AlignCenter)
+                label_image.move(146,10)
         except:
             print("imagen no encotrada ")
-            
-        label_Login = QLabel("Login",self)
-        label_Login.setStyleSheet("color: rgb(255,255,255);")
-        label_Login.setAlignment(Qt.AlignCenter)
-        label_Login.setFont(QFont("Times New Roman",25))
         
-        self.LineUsuario = QLineEdit(self)
-        self.LineUsuario.setFixedSize(400,40)
-        self.LineUsuario.setStyleSheet("border-radius: 20px;")
+        
+        self.LineUsuario = QLineEdit(frame_padre)
+        self.LineUsuario.setFixedSize(380,40)
+        self.LineUsuario.move(20,200)
+        self.LineUsuario.setStyleSheet("border-radius: 20px; border: 2px solid blue;")
         self.LineUsuario.addAction(QIcon("imagen/usuario1.png"),QLineEdit.LeadingPosition)
-        self.LineUsuario.setPlaceholderText("Nombre Usuario")
-        
+        self.LineUsuario.setPlaceholderText("UserName")
         self.LineUsuario.setFont(QFont("Times New Roman",14))
         
-        self.LinePassWord = QLineEdit(self)
-        self.LinePassWord.setFixedSize(400,40)
-        self.LinePassWord.setStyleSheet("QLineEdit{border-radius: 20px;}")
+        self.LinePassWord = QLineEdit(frame_padre)
+        self.LinePassWord.setFixedSize(380,40)
+        self.LinePassWord.move(20,280)
+        self.LinePassWord.setStyleSheet("QLineEdit{border-radius: 20px; border: 2px solid blue;}")
         self.LinePassWord.addAction(QIcon("imagen/cerrar-con-llave.png"),QLineEdit.LeadingPosition)
         self.LinePassWord.setPlaceholderText("Password")
         self.LinePassWord.setEchoMode(QLineEdit.Password)
         self.LinePassWord.setFont(QFont("Times New Roman",14))
         
-        self.button_login = QPushButton("Login",self)
-        self.button_login.setIcon(QIcon("imagen/iniciar-sesion.png"))
-        self.button_login.setIconSize(QSize(80,30))
+        self.show_Password = QCheckBox("Show", frame_padre)
+        self.show_Password.setFont(QFont("Times New Roman",12))
+        self.show_Password.move(300,330)
         
-        self.button_registrar = QPushButton("Registrar",self)
-        self.button_registrar.setIcon(QIcon("imagen/agregar-usuario.png"))
-        self.button_registrar.setIconSize(QSize(60,30))
+        self.button_login = QPushButton("Login",frame_padre)
+        self.button_login.setFont(QFont("Times New Roman",14))
+        self.button_login.setStyleSheet("background-color: rgb(255,255,255); border-radius: 5px; border: 2px solid rgb(39, 174, 96 )")
+        self.button_login.resize(100,40)
+        self.button_login.move(160,400)
+        self.button_login.installEventFilter(self)
         
-        self.button_gmail = QPushButton(self)
-        self.button_gmail.setIcon(QIcon("imagen/google.png"))
-        self.button_gmail.setIconSize(QSize(60,30))
-    
-        vertical_Layout_logo = QVBoxLayout()
-        vertical_Layout_logo.addSpacing(50)
-        vertical_Layout_logo.addWidget(label_image)
-        vertical_Layout_logo.addSpacing(20)
-        vertical_Layout_logo.addWidget(label_Login)
-       
-        layout_Line_usuario = QHBoxLayout()
-        layout_Line_usuario.addStretch()
-        layout_Line_usuario.addWidget(self.LineUsuario)
-        layout_Line_usuario.addStretch()
+        self.button_registrar = QPushButton("Sign in",frame_padre)
+        self.button_registrar.setStyleSheet("background-color: rgb(255,255,255); border-radius: 5px; border: 2px solid rgb(39, 174, 96 )")
+        self.button_registrar.setFont(QFont("Times New Roman", 14))
+        self.button_registrar.resize(100,40)
+        self.button_registrar.move(160,460)
+        self.button_registrar.installEventFilter(self)
+        self.button_registrar.clicked.connect(self.Registrar)
         
-        layout_Line_password = QHBoxLayout()
-        layout_Line_password.addStretch()
-        layout_Line_password.addWidget(self.LinePassWord)
-        layout_Line_password.addStretch()
+    def Registrar(self):
+        self.registro = RegistroUsuario()
+        self.registro.show()
         
-        layout_button_login = QHBoxLayout()
-        layout_button_login.addStretch()
-        layout_button_login.addWidget(self.button_login)
-        layout_button_login.addStretch()
-        
-        
-        layout_button_registro = QHBoxLayout()
-        layout_button_registro.addStretch()
-        layout_button_registro.addWidget(self.button_registrar)
-        layout_button_registro.addWidget(self.button_gmail)
-        layout_button_registro.addStretch()
-
-        formLayout = QFormLayout()
-        formLayout.addRow(vertical_Layout_logo)
-        
-        formLayout.addRow(layout_Line_usuario)
-        formLayout.addRow(layout_Line_password)
-        formLayout.addRow(layout_button_login)
-        formLayout.addRow(layout_button_registro)
-        formLayout.setVerticalSpacing(50)
-       
-        
-        
-        
-        Frame_Central.setLayout(formLayout)
-        
-   
-        
-        
-        
+ 
+    def eventFilter(self, source, event):
+        if event.type() == QEvent.MouseButtonPress:
+            if source == self.button_login:
+                self.button_login.setStyleSheet("background-color: rgb(133, 193, 233); border-radius: 5px; border: 2px solid rgb(39, 174, 96 );")
+            elif source == self.button_registrar:
+                print("Has hecho clic en el botón 2")
+        #elif event.type() == QEvent.M
+        elif event.type() == QEvent.Enter:
+            if source == self.button_login:
+                self.button_login.setStyleSheet("background-color: rgb(174, 214, 241); border-radius: 5px; border: 2px solid rgb(39, 174, 96 );")
+            elif event.type() == QEvent.Enter:
+                self.button_registrar.setStyleSheet("background-color: rgb(174, 214, 241); border-radius: 5px; border: 2px solid rgb(39, 174, 96 );")
+        elif event.type() == QEvent.Leave:
+            if source == self.button_login:
+                self.button_login.setStyleSheet("background-color: rgb(255, 255, 255); border-radius: 5px; border: 2px solid rgb(39, 174, 96 );")
+            elif source == self.button_registrar:
+                self.button_registrar.setStyleSheet("background-color: rgb(255, 255, 255); border-radius: 5px; border: 2px solid rgb(39, 174, 96 );")
+        return False
            
-        
+
+       
         
 if __name__=='__main__':
     App = QApplication(sys.argv)
     perfil = login()
-    perfil.setStyleSheet("QMainWindow{background-image: url(fondo 2.jpg);}")
+    perfil.setStyleSheet("""QMainWindow{background-image: url(luis3.jpg);background-repeat: no-repeat; background-size: cover; }
+                         background-position: center;""")
     perfil.show()
     sys.exit(App.exec_())
         
